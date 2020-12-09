5ce5156c613899dba6714747235a317ee49c8154
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-09 22:51:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -33,8 +33,8 @@ class Transfer
       end
     end
 
-    # def reject_transfer
-    #   self.status = "rejected"
-    #   "Transaction rejected. Please check your account balance."
-    # end
+    def reject_transfer
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
   end
