8ab665f7ba44a9d5b12afa59d6c97548342e28e9
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-09 22:48:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
     attr_reader :name
     attr_accessor :balance, :status
 
-    def initialize(name)
+    def initialize(sender)
       @name = name
       @balance = 1000
       @status = "pending"
